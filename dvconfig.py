f2f135889347d7eb905cc4319337fd8560f8e6d1
Guido Schmutz <guido.schmutz@trivadis.com>
2023-10-11 22:07:05 +0200

    Update dvconfig.py

--- dvconfig.py
+++ dvconfig.py
@@ -5,6 +5,7 @@ sample_data = [
 'data/dataverses/pums/pums.json',
 'data/dataverses/pums/datasets/2000pums5/2000pums5.json',
 'data/dataverses/dataverseno/dataverseno.json',
+'data/dataverses/dataverseno/datasets/tabular-sample-data/tabular-sample-data.json',
 'data/dataverses/open-source-at-harvard/open-source-at-harvard.json',
 'data/dataverses/open-source-at-harvard/dataverses/dataverse-project/dataverse-project.json',
 'data/dataverses/open-source-at-harvard/dataverses/dataverse-project/datasets/dataverse-irc-metrics/dataverse-irc-metrics.json',
@@ -12,8 +13,6 @@ sample_data = [
 'data/dataverses/ecastro/datasets/this-is-my-test-dataset/this-is-my-test-dataset.json',
 'data/dataverses/manchester/manchester.json',
 'data/dataverses/manchester/datasets/test-dataset/test-dataset.json',
-'data/dataverses/HCPDS/HCPDS.json',
-'data/dataverses/HCPDS/datasets/reproductive-health-laws-around-the-world/reproductive-health-laws-around-the-world.json',
 'data/dataverses/cms/cms.json',
 'data/dataverses/cms/datasets/cmssampledata/cmssampledata.json',
 'data/dataverses/scholcommlab/scholcommlab.json',
@@ -25,6 +24,8 @@ sample_data = [
 'data/dataverses/open-source-at-harvard/datasets/open-source-at-harvard/open-source-at-harvard.json',
 'data/dataverses/king/king.json',
 'data/dataverses/king/datasets/cause-of-death/cause-of-death.json',
+'data/dataverses/HCPDS/HCPDS.json',
+'data/dataverses/HCPDS/datasets/reproductive-health-laws-around-the-world/reproductive-health-laws-around-the-world.json',
 ]
 
 # put this back at line 6 once https://github.com/IQSS/dataverse/pull/6924 is merged
